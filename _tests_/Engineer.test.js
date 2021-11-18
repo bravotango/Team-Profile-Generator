449dfbdb1c4af0e3b5634815ce4378208ea5576a
bravotango <seattleriot@hotmail.com>
2021-11-17 21:43:19 -0800

    Tests are running

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,27 @@
+const Engineer = require("../lib/Engineer.js");
+
+describe("Engineer", () => {
+  const name = "Samir Nagheenanaj";
+  const id = 12;
+  const email = "samir.nagheenanaj@initech.com";
+  const gitHubUsername = "bravotango";
+  const returnedGitHub = `<a href="https://github.com/${gitHubUsername}">https://github.com/${gitHubUsername}</a>`;
+
+  const testEngineer = new Engineer(name, id, email, gitHubUsername);
+
+  it("Should return github linked account", () => {
+    expect(testEngineer.getGithub()).toBe(returnedGitHub);
+  });
+
+  it("Should return the Engineer name when requested", () => {
+    expect(testEngineer.getName()).toBe(name);
+  });
+
+  it("Should return the Engineer id when requested", () => {
+    expect(testEngineer.getId()).toBe(id);
+  });
+
+  it("Should return the Engineer email when requested", () => {
+    expect(testEngineer.getEmail()).toBe(email);
+  });
+});
